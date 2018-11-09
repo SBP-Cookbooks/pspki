1d829624ffed4059fa98553792b3f988d4c06ee5
Stephen Hoekstra <shoekstra@schubergphilis.com>
2018-11-09 14:20:31 +0100

    Switch to Apache 2 license

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name             'pspki'
-license          'All rights reserved'
 description      'Installs PKI PowerShell module (https://pspki.codeplex.com)'
 maintainer       'Schuberg Philis'
 maintainer_email 'cookbooks@schubergphilis.com'
+license          'Apache-2.0'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 issues_url       'https://github.com/sbp-cookbooks/pspki/issues'
