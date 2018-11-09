e2b3c0f26be882da00af4ba3e4f13cd3fd60e98c
Stephen Hoekstra <shoekstra@schubergphilis.com>
2018-11-09 14:22:57 +0100

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,12 @@
 name             'pspki'
-description      'Installs PKI PowerShell module (https://pspki.codeplex.com)'
 maintainer       'Schuberg Philis'
 maintainer_email 'cookbooks@schubergphilis.com'
 license          'Apache-2.0'
+description      'Installs PKI PowerShell module (https://www.powershellgallery.com/packages/PSPKI)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 issues_url       'https://github.com/sbp-cookbooks/pspki/issues'
 source_url       'https://github.com/sbp-cookbooks/pspki'
+chef_version     '>= 12.16'
 
 supports 'windows'
-chef_version '>= 12.1' if respond_to?(:chef_version)
