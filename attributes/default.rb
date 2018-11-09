023c267747818b4a95446c6f491ff037f4aa8d5d
Stephen Hoekstra <shoekstra@schubergphilis.com>
2018-11-09 13:04:16 +0100

    Install modules from PS Gallery
    Signed-off-by: Stephen Hoekstra <shoekstra@schubergphilis.com>

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,7 +7,7 @@
 # Created by: Stephen Hoekstra <shoekstra@schubergphilis.com>
 #
 
-default['pspki']['pscx_package_name']  = 'PowerShell Community Extensions 3.2.0'
-default['pspki']['pscx_source_url']    = 'https://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=pscx&DownloadId=923562&FileTime=130585918034470000&Build=21040'
-default['pspki']['pspki_package_name'] = 'PowerShell PKI Module'
-default['pspki']['pspki_source_url']   = 'http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=pspki&DownloadId=1600115&FileTime=131151584222300000&Build=21031'
+default['pspki']['pscx_install'] = true
+default['pspki']['pscx_version'] = nil
+default['pspki']['pspki_install'] = true
+default['pspki']['pspki_version'] = nil
